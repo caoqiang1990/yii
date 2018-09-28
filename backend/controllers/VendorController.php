1cfe4117bd095251340910f092d515769668441a
caoqiang1990 <351266168@qq.com>
2018-09-28 11:02:32 +0800

    后台页面修改

--- backend/controllers/VendorController.php
+++ backend/controllers/VendorController.php
@@ -9,6 +9,10 @@ use yii\data\ActiveDataProvider;
 
 class VendorController extends Controller
 {
+  /**
+   * 供应商列表
+   * @return [type] [description]
+   */
   public function actionIndex()
   {
     $model=new Vendor();
@@ -23,6 +27,21 @@ class VendorController extends Controller
     ]);    
   }
 
+  /**
+   * 供应商添加
+   * @return [type] [description]
+   */
+  public function actionCreate()
+  {
+    return $this->render('create',[]);
+  }
+
+
+
+  public function actionTest()
+  {
+    var_dump(Yii::$app->user->identity);die;
+  }
 
 
 }
