f67b65a4fe016635a2b7e926360bd8dc8b490cc0
caoqiang1990 <351266168@qq.com>
2018-11-22 14:47:01 +0800

    modify function

--- backend/views/supplier/uploadxls.php
+++ backend/views/supplier/uploadxls.php
@@ -31,7 +31,7 @@ $this->params['breadcrumbs'][] = $this->title;
                     //网上很多地方都没详细说明回调触发事件，其实fileupload为上传成功后触发的，三个参数，主要是第二个，有formData，jqXHR以及response参数，上传成功后返回的ajax数据可以在response获取
                     'pluginEvents' => [
                         'fileuploaded' => "function (object,data){ 
-                            $("input[name='UploadForm[imageFile]']").val(data.filepath);
+                            $("input[name='UploadForm\[imageFile\]']").val(data.filepath);
                             alert('上传成功');
                         }",
                         //错误的冗余机制
