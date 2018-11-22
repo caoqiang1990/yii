b01c638183d7e7df7ac7d7d18f84d826a0c77414
caoqiang1990 <351266168@qq.com>
2018-11-22 15:08:11 +0800

    modify function

--- backend/views/supplier/uploadxls.php
+++ backend/views/supplier/uploadxls.php
@@ -18,7 +18,7 @@ $this->params['breadcrumbs'][] = $this->title;
 
         <?php $form = ActiveForm::begin() ?>
             <?php
-                echo $form->field($model, 'imageFile','options'=>['class'=>'filepath'])->widget(FileInput::classname(),[
+                echo $form->field($model, 'imageFile')->widget(FileInput::classname(),[
                     'options' => [
                         'module' => 'Supplier',
                         'multipe' => false,
