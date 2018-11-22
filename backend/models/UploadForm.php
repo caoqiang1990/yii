2af85587321c759c2bcd9ce3cedba855e65f62ac
caoqiang1990 <351266168@qq.com>
2018-11-22 15:12:26 +0800

    modify function

--- backend/models/UploadForm.php
+++ backend/models/UploadForm.php
@@ -23,11 +23,11 @@ class UploadForm extends Model
     public function upload()
     {
         if ($this->validate()) {
-            $path = Yii::getAlias('@uploadPath') . '/' . date("Ymd");
+            $path = \Yii::getAlias('@uploadPath') . '/' . date("Ymd");
             if (!is_dir($path) || !is_writable($path)) {
                 FileHelper::createDirectory($path, 0777, true);
             }
-            $filePath = $path . '/' . Yii::$app->request->post('model', '') . '_' . md5(uniqid() . mt_rand(10000, 99999999)) . '.' . $this->imageFile->extension;
+            $filePath = $path . '/' . \Yii::$app->request->post('model', '') . '_' . md5(uniqid() . mt_rand(10000, 99999999)) . '.' . $this->imageFile->extension;
 
             if($this->imageFile->saveAs($filePath)) {
                 //如果上传成功，保存附件信息到数据库。TODO
