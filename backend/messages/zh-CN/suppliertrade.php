02ce765f9a9b571b476b7415b92d836eb4758cec
caoqiang1990 <351266168@qq.com>
2018-11-29 11:02:41 +0800

    bug fixed

--- backend/messages/zh-CN/suppliertrade.php
+++ backend/messages/zh-CN/suppliertrade.php
@@ -13,7 +13,7 @@ return [
   'Delete' => '删除',
   'Are you sure you want to delete this item?' => '确认要删除吗?',
   'Order No' => '排序',
-
+  'Update Supplier Trade: ' => '更新行业',
 ];
 
 
--- backend/views/supplier-trade/update.php
+++ backend/views/supplier-trade/update.php
@@ -4,11 +4,11 @@ use yii\helpers\Html;
 
 /* @var $this yii\web\View */
 /* @var $model backend\models\SupplierTrade */
-
-$this->title = 'Update Supplier Trade: ' . $model->id;
-$this->params['breadcrumbs'][] = ['label' => 'Supplier Trades', 'url' => ['index']];
+$title = Yii::t('trade','Update Supplier Trade: ');
+$this->title = $title . $model->id;
+$this->params['breadcrumbs'][] = ['label' => Yii::t('trade','Supplier Trades'), 'url' => ['index']];
 $this->params['breadcrumbs'][] = ['label' => $model->id, 'url' => ['view', 'id' => $model->id]];
-$this->params['breadcrumbs'][] = 'Update';
+$this->params['breadcrumbs'][] = Yii::t('trade','Update');
 ?>
 <div class="supplier-trade-update">
 
