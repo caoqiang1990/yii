e14c9e1bdf417815fca75e910b00450acf29edf1
fubaowei <fubaowei@hotmail.com>
2018-12-28 15:26:17 +0800

    gys

--- frontend/views/supplierform/update.php
+++ frontend/views/supplierform/update.php
@@ -4,15 +4,15 @@ use yii\helpers\Html;
 
 /* @var $this yii\web\View */
 /* @var $model backend\models\Suppliers */
-$title = Yii::t('suppliers', 'Update Suppliers: ');
-$this->title = $title . $model->id;
+$this->title = Yii::t('supplier', 'supplierbasic');
+//$this->title = $title . $model->id;
 $this->params['breadcrumbs'][] = ['label' => Yii::t('suppliers', 'Suppliers'), 'url' => ['index']];
 $this->params['breadcrumbs'][] = ['label' => $model->id, 'url' => ['view', 'id' => $model->id]];
 $this->params['breadcrumbs'][] = Yii::t('suppliers', 'Update');
 ?>
 <div class="suppliers-update">
 
-    <h1><?= Html::encode($this->title) ?></h1>
+    <table style="width:100%"><tr><td colspan="3"><h1><?= Html::encode($this->title) ?></h1></td><td><img src="./images/logo.jpg" style="width:110px;height:110px;align:right;" /></td></tr></table>
 
     <?= $this->render('_upform', [
         'model' => $model,
