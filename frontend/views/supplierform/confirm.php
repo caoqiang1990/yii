39ece9aa9c3dcb85b7b6bbea01378d8ed53973d7
fubaowei <fubaowei@hotmail.com>
2018-12-28 15:06:42 +0800

    gys

--- frontend/views/supplierform/confirm.php
+++ frontend/views/supplierform/confirm.php
@@ -11,10 +11,9 @@ $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="suppliers-view">
 
-    <h1><?= Html::encode($this->title) ?></h1>
-
-    <p>
-        <?= Html::tag('h2', '信息已成功提交！', ['class' => 'confirmtext']); ?>
-    </p>
+    <table style="width:100%">
+      <tr><td colspan="2" style="width:35%"></td><td><h1><?= Html::tag('h2', '信息已成功提交！', ['class' => 'confirmtext']); ?></td><td colspan="2"></td></tr>
+      <tr><td colspan="2"></td><td></td><td colspan="2"></td></tr>
+    </table>
 
 </div>
