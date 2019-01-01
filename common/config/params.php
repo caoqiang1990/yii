034d100146922f4a5442215fe1c7090f1f4cb014
caoqiang1990 <351266168@qq.com>
2019-01-01 16:10:39 +0800

    modify

--- common/config/params.php
+++ common/config/params.php
@@ -3,5 +3,5 @@ return [
     'adminEmail' => 'admin@example.com',
     'supportEmail' => '351266168@qq.com',
     'user.passwordResetTokenExpire' => 3600,
-    'assetDomain' => 'http://localhost:8080/uploads/upload/',
+    'assetDomain' => 'http://admin.gys.aimergroup.com:8090//uploads/upload/',
 ];
