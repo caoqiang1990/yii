c010bb07578a6dbce8b512f7f41a92ff57bf6d0d
caoqiang1990 <351266168@qq.com>
2019-01-04 20:25:50 +0800

    modify

--- backend/views/site/login.php
+++ backend/views/site/login.php
@@ -10,46 +10,61 @@ $this->title = '供应商管理系统';
 
 $fieldOptions1 = [
     'options' => ['class' => 'form-group has-feedback'],
-    'inputTemplate' => "{input}<span class='glyphicon glyphicon-envelope form-control-feedback'></span>"
+    'inputTemplate' => "{input}<span class='glyphicon glyphicon-user form-control-feedback'></span>"
 ];
 
 $fieldOptions2 = [
     'options' => ['class' => 'form-group has-feedback'],
     'inputTemplate' => "{input}<span class='glyphicon glyphicon-lock form-control-feedback'></span>"
 ];
+
+$css = <<<CSS
+    .clear{
+        clear:both;
+    }
+    .forget{
+        position:relative;
+    }
+    .remember{
+        padding-top: 5px;
+    }
+CSS;
+$this->registerCss($css);
 ?>
 
-<div class="login-box">
+<div class="login-box" style="width:100%;margin:auto;position: absolute;bottom:100px">
     <div class="login-logo">
     </div>
     <!-- /.login-logo -->
-    <div class="login-box-body">
-        <p class="login-box-msg" style="font-size: 22px">供应商管理系统</p>
+    <div class="login-box-body" style="width:900px;margin:auto">
+        <!-- <p class="login-box-msg" style="font-size: 22px">供应商管理系统</p> -->
 
-        <?php $form = ActiveForm::begin(['id' => 'login-form', 'enableClientValidation' => false]); ?>
-
+        <?php $form = ActiveForm::begin(['id' => 'login-form', 'enableClientValidation' => false,'options'=>['class'=>'form-inline']]); ?>
+        <div class="col-sm-4">    
         <?= $form
             ->field($model, 'username', $fieldOptions1)
             ->label(false)
             ->textInput(['placeholder' => $model->getAttributeLabel('username')]) ?>
-
+        </div>
+        <div class="col-sm-4">    
         <?= $form
             ->field($model, 'password', $fieldOptions2)
             ->label(false)
             ->passwordInput(['placeholder' => $model->getAttributeLabel('password')]) ?>
-
-        <div class="row">
-            <div class="col-xs-8">
-                <?= $form->field($model, 'rememberMe')->checkbox() ?>
-            </div>
-            <!-- /.col -->
-            <div class="col-xs-4">
-                <?= Html::submitButton('登录', ['class' => 'btn btn-primary btn-block btn-flat', 'name' => 'login-button']) ?>
-            </div>
-            <!-- /.col -->
         </div>
+        <div class="col-xs-2">
+            <?= $form->field($model, 'rememberMe',['options'=>['class'=>'remember']])->checkbox() ?>
+        </div>
+            <!-- /.col -->
+        <div class="col-xs-2">
+            <?= Html::submitButton('登录', ['class' => 'btn btn-primary btn-block btn-flat', 'name' => 'login-button']) ?>
+        </div>
+
+            <!-- /.col -->
         <?php ActiveForm::end(); ?>
-        <?= Html::a('忘记密码', ['site/reset']) ?>
+
+        <div class="clear"></div>
     </div>
+  
     <!-- /.login-box-body -->
 </div><!-- /.login-box -->
