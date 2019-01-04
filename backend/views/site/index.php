633d19fbb6b133b50c5428d2644eadd2a020947e
caoqiang1990 <351266168@qq.com>
2019-01-04 16:54:31 +0800

    modify

--- backend/views/site/index.php
+++ backend/views/site/index.php
@@ -1,7 +1,72 @@
 <?php
+use yii\web\View;
 
 $this->title = '';
 
+$css = <<<CSS
+  ol li{
+    font-size: 18px;
+    margin-bottom: 10px
+  }
+  h4{
+    font-weight: bold;
+  }
+
+CSS;
+
+$this->registerCss($css);
 ?>
-<h4>欢迎登录爱慕供应商系统</h4>
-<img src="/static/images/paper.jpg" style="width:100%">
+<div class="container-fluid">
+<div class="row">
+<h1>欢迎登录爱慕供应商系统</h1>
+</div>
+  <div class="row">
+      <h4>供应商管理系统信息完善时间要求：</h4>
+      <ol>
+        <li>2018年1月20日前，将2018年及之前未录入系统的合作供应商补录完整（路径：“供应商名录变更->新增供应商”）。</li>
+        <li>2018年1月31日前，将2018年合作信息补录完整（路径：“供应商名录变更->变更合作信息”下的“2018年合同金额”和“2018年交易金额”）。</li>
+        <li>2018年1月31日前，完成2018年供应商等级评价并在系统中更新完毕（路径：“供应商名录变更->变更合作信息”下的“供应商等级”）。</li>
+      </ol>
+  </div>
+  <div class="row">
+    <div class="col-xs-12">
+    <h4>使用指南：</h4>
+    <ol>
+      <li>
+        共享范围释义：
+        <ol>
+          <li>供应商分为“集团共享”供应商和“部门共享”供应商。</li>
+          <li>新增供应商时系统默认为“集团共享”，如为保密供应商（如特殊的原材料供应商），请在“供应商名录变更->变更基本信息”或“供应商名录变更->审核基本信息”处调整为“部门共享”。</li>
+          <li>“供应商名录查询->集团供应商”中不显示其他部门的“部门共享”供应商。</li>
+        </ol>
+      </li>
+      <li>
+        <p>个人信息修改：</p>
+        <img src="/static/images/intro/1.png">
+      </li>
+      <li>
+          <p>供应商名录查询：</p>
+          <img src="/static/images/intro/2.png" style="width:100%">
+      </li>
+      <li>
+          <p>供应商名录变更->新增供应商</p>
+          <img src="/static/images/intro/3.png" style="width:100%">
+          <img src="/static/images/intro/4.png" style="width:100%">
+      </li>
+      <li>
+          <p>供应商名录变更->变更基本信息</p>
+          <img src="/static/images/intro/5.png" style="width:100%">
+      </li>
+      <li>
+          <p>供应商名录变更->审核基本信息</p>
+          <img src="/static/images/intro/6.png" style="width:100%">
+      </li>
+      <li>
+          <p>供应商名录变更->变更合作信息</p>
+          <img src="/static/images/intro/7.png" style="width:100%">
+      </li>
+      <li><p>技术支持：大数据信息中心010-84776011</p></li>
+    </ol>
+  </div>
+  </div>
+</div>
