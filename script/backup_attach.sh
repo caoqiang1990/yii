d30a21776ea91856a829d91c4ae6853b24677a12
caoqiang1990 <351266168@qq.com>
2019-03-20 10:16:52 +0800

    备份附件

--- script/backup_attach.sh
+++ script/backup_attach.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-mypath='~/backup'                        #日志备份到该目录下，定义变量使用单引号
 
-mylog = '/var/www/yii/backend/web/uploads'
+mypath='~/backup'                               #日志备份到该目录下，定义变量使用单引号
+mylog='/var/www/yii/backend/web/uploads'
+time=`date +%Y%m%d%H%M%S`                       #时间戳，执行命令使用``,esc下面的
 
-time=`date +%Y%m%d%H%M%S`   #时间戳，执行命令使用``,esc下面的
-
-tar zcvf ${mylog} ${mypath}/${time}.tar.gz     #备份附件到到/backup路径下
+tar zcvf ${mylog} ${mypath}/${time}.tar.gz      #备份附件到到/backup路径下
