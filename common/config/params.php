3d927f69ea0aa6ec1f2a0e198b706954267e67d7
caoqiang1990 <351266168@qq.com>
2019-03-27 10:16:09 +0800

    图片域名修改

--- common/config/params.php
+++ common/config/params.php
@@ -3,5 +3,7 @@ return [
     'adminEmail' => 'admin@example.com',
     'supportEmail' => '351266168@qq.com',
     'user.passwordResetTokenExpire' => 3600,
-    'assetDomain' => 'http://admin.gys.aimergroup.com:8090//uploads/upload/',
+    //'assetDomain' => 'http://admin.gys.aimergroup.com:8090/uploads/upload/',
+    'assetDomain' => 'http://gys.caoqiang1990.com.cn/uploads/upload/',
+
 ];
