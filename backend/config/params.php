394344b731d87d516af6d551c4eadc3daefcfb8a
caoqiang1990 <351266168@qq.com>
2019-04-15 10:24:54 +0800

    配置修改

--- backend/config/params.php
+++ backend/config/params.php
@@ -3,7 +3,7 @@ return [
     'adminEmail' => 'admin@example.com',
     'assetDomain' => 'http://admin.gys.aimergroup.com:8090/uploads/upload/',
     //'assetDomain' => 'http://gys.caoqiang1990.com.cn/uploads/upload/',
-    'frontUrl' => 'http://gys.aimergroup.com:8090/',
+    'frontUrl' => 'http://gys.aimergroup.com:8090',
     //'frontUrl_test' => 'http://front.gys.com',
     'frontUrl_test' => 'http://front.caoqiang1990.com.cn',
 ];
