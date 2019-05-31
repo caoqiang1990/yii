d7e6382ee5f87660b9c4abfa4e455e5a0a54faac
caoqiang1990 <351266168@qq.com>
2019-05-31 08:58:14 +0800

    sw

--- backend/controllers/SupplierDetailController.php
+++ backend/controllers/SupplierDetailController.php
@@ -216,6 +216,13 @@ class SupplierDetailController extends Controller
         }
         if ($model->load($post) && $model->save()) {
             AdminLog::saveLog('supplierdetail', 'create', $model->getByID($model->primaryKey), $model->primaryKey);
+            //调用swoole客户端
+            $client = new \swoole_client(SWOOLE_SOCK_TCP);
+            if (!$client->connect('127.0.0.1', 9503)) {
+                exit("connect failed. Error: {$client->errCode}\n");
+            }
+            $client->send($model->primaryKey);
+            $client->close();
             return $this->redirect(['/supplier/admin-index']);
         }
 
