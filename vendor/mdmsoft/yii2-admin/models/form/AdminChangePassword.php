1920ad3388223703b73fa7c0d3cd0f94126e02d2
caoqiang1990 <351266168@qq.com>
2019-06-10 13:25:19 +0800

    modify

---
+++ vendor/mdmsoft/yii2-admin/models/form/AdminChangePassword.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace mdm\admin\models\form;
+
+use Yii;
+use mdm\admin\models\User;
+use yii\base\Model;
+
+/**
+ * Description of ChangePassword
+ *
+ * @author Misbahul D Munir <misbahuldmunir@gmail.com>
+ * @since 1.0
+ */
+class AdminChangePassword extends Model
+{
+    public $oldPassword;
+    public $newPassword;
+    public $retypePassword;
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['newPassword', 'retypePassword'], 'required'],
+            //[['oldPassword'], 'validatePassword'],
+            [['newPassword'], 'string', 'min' => 6],
+            [['retypePassword'], 'compare', 'compareAttribute' => 'newPassword'],
+        ];
+    }
+
+    /**
+     * Validates the password.
+     * This method serves as the inline validation for password.
+     */
+    public function validatePassword()
+    {
+        /* @var $user User */
+        $user = Yii::$app->user->identity;
+        if (!$user || !$user->validatePassword($this->oldPassword)) {
+            $this->addError('oldPassword', '旧密码不正确.');
+        }
+    }
+
+    /**
+     * Change password.
+     *
+     * @return User|null the saved model or null if saving fails
+     */
+    public function change($id)
+    {
+        if ($this->validate()) {
+            /* @var $user User */
+            if (!$id) {
+                return false;
+            }
+            $user = User::findIdentity($id);
+            $user->setPassword($this->newPassword);
+            $user->generateAuthKey();
+            if ($user->save()) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * 对应中文
+     * @return [type] [description]
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => Yii::t('rbac-admin','ID'),
+            'username' => Yii::t('rbac-admin','Username'),
+            'created_at' => Yii::t('rbac-admin','Created At'),
+            'email' => Yii::t('rbac-admin','Email'),
+            'status' => Yii::t('rbac-admin','Status'),
+            'oldPassword' => Yii::t('rbac-admin','oldPassword'),
+            'newPassword' => Yii::t('rbac-admin','newPassword'),
+            'retypePassword' => Yii::t('rbac-admin','retypePassword'),
+            'change' => Yii::t('rbac-admin','change'),
+        ];
+    }
+}
