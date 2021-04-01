6867fe1791eb4166ec4f711a457364b208e28911
caoqiang1990 <351266168@qq.com>
2021-04-01 15:58:44 +0800

    bug fixed

--- backend/models/TemplateRecord.php
+++ backend/models/TemplateRecord.php
@@ -15,213 +15,213 @@ use yii\helpers\ArrayHelper;
  */
 class TemplateRecord extends ActiveRecord
 {
-    public $result_1;
-    public $result_2;
-    public $result_3;
-    public $result_4;
-    public $result_5;
-    public $result_6;
-    public $result_7;
-    public $result_8;
-    public $result_9;
-    public $result_10;
-    public $result_11;
-    public $result_12;
-    public $result_13;
-    public $result_14;
-    public $result_15;
-    public $result_16;
-    public $result_17;
-    public $result_18;
-    public $result_19;
-    public $result_20;
+  public $result_1;
+  public $result_2;
+  public $result_3;
+  public $result_4;
+  public $result_5;
+  public $result_6;
+  public $result_7;
+  public $result_8;
+  public $result_9;
+  public $result_10;
+  public $result_11;
+  public $result_12;
+  public $result_13;
+  public $result_14;
+  public $result_15;
+  public $result_16;
+  public $result_17;
+  public $result_18;
+  public $result_19;
+  public $result_20;
 
-    public $count;
+  public $count;
 
-    /**
-     * @inheritdoc
-     */
-    public function rules()
-    {
-        return [
-            [['template_id', 'question_id', 'sid', 'result_1', 'result_2', 'result_3', 'result_4', 'result_5', 'result_6', 'result_7', 'result_8', 'result_9', 'result_10', 'result_11', 'result_12', 'result_13', 'result_14', 'result_15', 'result_16', 'result_17', 'result_18', 'result_19', 'result_20', 'result', 'count'], 'safe'],
-            [['reason', 'total', 'operator', 'project', 'department'], 'required'],
+  /**
+   * @inheritdoc
+   */
+  public function rules()
+  {
+    return [
+        [['template_id', 'question_id', 'sid', 'result_1', 'result_2', 'result_3', 'result_4', 'result_5', 'result_6', 'result_7', 'result_8', 'result_9', 'result_10', 'result_11', 'result_12', 'result_13', 'result_14', 'result_15', 'result_16', 'result_17', 'result_18', 'result_19', 'result_20', 'result', 'count'], 'safe'],
+        [['reason', 'total', 'operator', 'project', 'department'], 'required'],
 
-        ];
-    }
+    ];
+  }
 
-    /**
-     * @inheritdoc
-     */
-    public static function tableName()
-    {
-        return '{{%template_record}}';
-    }
+  /**
+   * @inheritdoc
+   */
+  public static function tableName()
+  {
+    return '{{%template_record}}';
+  }
 
-    /**
-     * 行为
-     * @return [type] [description]
-     */
-    public function behaviors()
-    {
-        return [
-            TimestampBehavior::className(),
-            BlameableBehavior::className(),
-        ];
-    }
+  /**
+   * 行为
+   * @return [type] [description]
+   */
+  public function behaviors()
+  {
+    return [
+        TimestampBehavior::className(),
+        BlameableBehavior::className(),
+    ];
+  }
 
-    /**
-     * Name: beforeSave
-     * User: aimer
-     * Date: 2019/9/27
-     * Time: 上午10:52
-     * @param bool $insert
-     * @return bool
-     */
-    public function beforeSave($insert)
-    {
-        if (parent::beforeSave($insert)) {
-            if ($this->isNewRecord) {
-                if ($this->count) {
-                    $result = [];
-                    for ($i = 1; $i <= $this->count; $i++) {
-                        $result["result_$i"] = $this->{"result_$i"};
-                    }
-                    $this->result = serialize($result);
-                }
-            }
-            return true;
-        } else {
-            return false;
+  /**
+   * Name: beforeSave
+   * User: aimer
+   * Date: 2019/9/27
+   * Time: 上午10:52
+   * @param bool $insert
+   * @return bool
+   */
+  public function beforeSave($insert)
+  {
+    if (parent::beforeSave($insert)) {
+      if ($this->isNewRecord) {
+        if ($this->count) {
+          $result = [];
+          for ($i = 1; $i <= $this->count; $i++) {
+            $result["result_$i"] = $this->{"result_$i"};
+          }
+          $this->result = serialize($result);
         }
+      }
+      return true;
+    } else {
+      return false;
     }
+  }
 
-    public function afterFind()
-    {
-        $result = unserialize($this->result);
-        $count = count($result);
-        for ($i = 1; $i <= $count; $i++) {
-            $this->{"result_$i"} = $result["result_$i"];
-        }
+  public function afterFind()
+  {
+    $result = unserialize($this->result);
+    $count = count($result);
+    for ($i = 1; $i <= $count; $i++) {
+      $this->{"result_$i"} = $result["result_$i"];
     }
+  }
 
-    /**
-     * Name: getByQuestionId
-     * User: aimer
-     * Date: 2019/6/3
-     * Time: 上午8:51
-     * @param $question_id
-     * @return bool
-     */
-    public static function getByTemplateId($template_id, $question_id)
-    {
-        if (!$template_id) {
-            return false;
-        }
-        if (!$question_id) {
-            return false;
-        }
-        $where['template_id'] = $template_id;
-        $where['question_id'] = $question_id;
-        $where['created_by'] = Yii::$app->user->identity->id;
-        $one = self::find()->where($where)->one();
-        return $one;
+  /**
+   * Name: getByQuestionId
+   * User: aimer
+   * Date: 2019/6/3
+   * Time: 上午8:51
+   * @param $question_id
+   * @return bool
+   */
+  public static function getByTemplateId($template_id, $question_id)
+  {
+    if (!$template_id) {
+      return false;
     }
+    if (!$question_id) {
+      return false;
+    }
+    $where['template_id'] = $template_id;
+    $where['question_id'] = $question_id;
+    $where['created_by'] = Yii::$app->user->identity->id;
+    $one = self::find()->where($where)->one();
+    return $one;
+  }
 
-    /**
-     * Name: getQuestionRecordById
-     * User: aimer
-     * Date: 2019/6/3
-     * Time: 上午8:51
-     * @param $question_id
-     * @return array|bool|ActiveRecord[]
-     */
-    public static function getQuestionRecordById($template_id)
-    {
-        if (!$template_id) {
-            return false;
-        }
-        $where['template_id'] = $template_id;
-        $lists = self::find()->where($where)->all();
-        return $lists;
+  /**
+   * Name: getQuestionRecordById
+   * User: aimer
+   * Date: 2019/6/3
+   * Time: 上午8:51
+   * @param $question_id
+   * @return array|bool|ActiveRecord[]
+   */
+  public static function getQuestionRecordById($template_id)
+  {
+    if (!$template_id) {
+      return false;
     }
+    $where['template_id'] = $template_id;
+    $lists = self::find()->where($where)->all();
+    return $lists;
+  }
 
-    /**
-     * Name: hasQuestionRecord
-     * User: aimer
-     * Date: 2019/6/3
-     * Time: 上午9:03
-     * @param string $question_id
-     * @param string $user_id
-     * @return bool
-     */
-    public function hasTemplateRecord($template_id = '', $question_id = '', $user_id = '')
-    {
-        if (!$template_id) {
-            return false;
-        }
-        if (!$question_id) {
-            return false;
-        }
-        if (!$user_id) {
-            return false;
-        }
-        $where['template_id'] = $template_id;
-        $where['question_id'] = $question_id;
-        $where['created_by'] = $user_id;
-        $result = self::find()->where($where)->all();
-        if (empty($result)) {
-            return false;
-        }
-        return true;
+  /**
+   * Name: hasQuestionRecord
+   * User: aimer
+   * Date: 2019/6/3
+   * Time: 上午9:03
+   * @param string $question_id
+   * @param string $user_id
+   * @return bool
+   */
+  public function hasTemplateRecord($template_id = '', $question_id = '', $user_id = '')
+  {
+    if (!$template_id) {
+      return false;
     }
+    if (!$question_id) {
+      return false;
+    }
+    if (!$user_id) {
+      return false;
+    }
+    $where['template_id'] = $template_id;
+    $where['question_id'] = $question_id;
+    $where['created_by'] = $user_id;
+    $result = self::find()->where($where)->all();
+    if (empty($result)) {
+      return false;
+    }
+    return true;
+  }
 
-    /**
-     * {@inheritdoc}
-     */
-    public function attributeLabels()
-    {
-        return [
-            'operator' => Yii::t('template', 'operator'),
-            'total' => Yii::t('template', 'total'),
-            'is_satisfy' => Yii::t('template', 'is_satisfy'),
-            'project' => Yii::t('template', 'project'),
-            'reason' => Yii::t('template', 'reason'),
-            'department' => Yii::t('template', 'department'),
-            'template_id' => Yii::t('template', 'template_id'),
-            'question_id' => Yii::t('template', 'question_id'),
-            'created_at' => Yii::t('template', 'created_at'),
-            'updated_at' => Yii::t('template', 'updated_at'),
-            'sid' => Yii::t('template', 'sid'),
-        ];
-    }
+  /**
+   * {@inheritdoc}
+   */
+  public function attributeLabels()
+  {
+    return [
+        'operator' => Yii::t('template', 'operator'),
+        'total' => Yii::t('template', 'total'),
+        'is_satisfy' => Yii::t('template', 'is_satisfy'),
+        'project' => Yii::t('template', 'project'),
+        'reason' => Yii::t('template', 'reason'),
+        'department' => Yii::t('template', 'department'),
+        'template_id' => Yii::t('template', 'template_id'),
+        'question_id' => Yii::t('template', 'question_id'),
+        'created_at' => Yii::t('template', 'created_at'),
+        'updated_at' => Yii::t('template', 'updated_at'),
+        'sid' => Yii::t('template', 'sid'),
+    ];
+  }
 
-    /**
-     * Name: getTemplateRecordByParams
-     * User: aimer
-     * Date: 2019/9/29
-     * Time: 下午2:08
-     * @param array $params
-     * @return array|bool|ActiveRecord[]
-     */
-    public function getTemplateRecordByParams($params = [])
-    {
-        if (empty($params)) {
-            return false;
-        }
-        $records = self::find()->where($params)->asArray()->all();
-        return $records;
+  /**
+   * Name: getTemplateRecordByParams
+   * User: aimer
+   * Date: 2019/9/29
+   * Time: 下午2:08
+   * @param array $params
+   * @return array|bool|ActiveRecord[]
+   */
+  public function getTemplateRecordByParams($params = [])
+  {
+    if (empty($params)) {
+      return false;
     }
+    $records = self::find()->where($params)->andWhere(['>','created_at','1577808000'])->asArray()->all();
+    return $records;
+  }
 
-    public static function getSuppliers()
-    {
-        $sids = self::find()->select("sid")->asArray()->all();
-        $list = array();
-        foreach ($sids as $id) {
-            $supplier = Supplier::getSupplierById($id);
-            $list[] = $supplier;
-        }
-        $result = ArrayHelper::map($list, 'id', 'name');
-        return $result;
+  public static function getSuppliers()
+  {
+    $sids = self::find()->select("sid")->asArray()->all();
+    $list = array();
+    foreach ($sids as $id) {
+      $supplier = Supplier::getSupplierById($id);
+      $list[] = $supplier;
     }
+    $result = ArrayHelper::map($list, 'id', 'name');
+    return $result;
+  }
 }
--- console/controllers/QuestionController.php
+++ console/controllers/QuestionController.php
@@ -15,7 +15,7 @@ class QuestionController extends Controller {
   {
     //获取所有评价
     $count = 0;
-    $lists = Question::find()->select('id,template_id,sid,status')->where(['template_id' => [7,8,9,10]])->asArray()->all();
+    $lists = Question::find()->select('id,template_id,sid,status')->where(['template_id' => [7,8,9,10]])->andWhere(['>','start_date','2021-01-01 00:00'])->asArray()->all();
     foreach ($lists as $list) {
       $data = array();
       $supplierModel = '';
@@ -87,17 +87,93 @@ class QuestionController extends Controller {
             echo "评价id{$list['id']}-不成功".PHP_EOL;
           }
         }
-//        $client = new \swoole_client(SWOOLE_SOCK_TCP);
-//        if (!$client->connect('127.0.0.1', 9503)) {
-//          exit("connect failed. Error: {$client->errCode}\n");
-//        }
-//        $data['id'] = $list['sid'];
-//        $data['level'] = $level_id;
-//        $data = serialize($data);
-//        $client->send($data);
-//        $client->close();
       }
     }
     echo "总数为{$count}".PHP_EOL;
   }
+
+  /**
+   * Name: actionSync2020
+   * User: aimer
+   * Date: 2021/4/1
+   * Time: 下午2:19
+   * @throws yii\web\NotFoundHttpException
+   */
+  public function actionSync2020()
+  {
+    //获取所有评价
+    $count = 0;
+    $lists = Question::find()->select('id,template_id,sid,status')->where(['template_id' => [7,8,9,10]])->andWhere(['>','start_date','2020-01-01 00:00'])->groupBy('sid')->asArray()->all();
+    foreach ($lists as $list) {
+      $data = array();
+      $supplierModel = '';
+      $count++;
+      if ($list['status'] != 3) {
+        echo "评价id为{$list['id']}-失败".PHP_EOL;
+      } else {
+        //收集答案，评测
+        $level = SupplierLevel::getLevels();
+        $levelFlip = array_flip($level);
+        $total = 0;
+        $result = 0;
+        $num = 0;
+        $templateRecordModel = new TemplateRecord();
+        $where['sid'] = $list['sid'];
+        $where['template_id'] = [7,8,9,10];
+        $records = $templateRecordModel->getTemplateRecordByParams($where);
+        if (!$records) {
+          echo "评价为空，id为{$list['id']}".PHP_EOL;
+          continue;
+        }
+        //总分
+        $num = count($records);
+        foreach ($records as $record) {
+          $total += $record['total'];
+        }
+        $result = $total / $num;
+        if ($result <= 0 || ($result > 0 && $result < 60)) {
+          $level = '不合格';
+        }
+        if ($result >= 60 && $result < 90) {
+          $level = '合格';
+        }
+        if ($result >= 90 && $result <= 100) {
+          $level = '优秀';
+        }
+        if ($result > 100) {
+          $level = '优秀';
+        }
+        $level_id = $levelFlip["{$level}"];
+        //修改供应商评价等级
+        $sql = "SELECT * FROM supplier WHERE status='10' AND id={$list['sid']}";
+        $supplier = Yii::$app->db->createCommand($sql)->queryAll();
+        foreach ($supplier as $v) {
+          $detail = SupplierDetail::getBySid($v['id']);
+          if ($detail && ($detail['one_level_department'] == $v['department'])) {
+            $supplierModel = Supplier::getByID($v['id']);
+            $supplierModel->scenario = 'sync';
+            if ($detail['cate_id1']) {
+              //$supplierModel->cate_id1 = $detail['cate_id1'];
+            }
+            if ($detail['cate_id2']) {
+              //$supplierModel->cate_id2 = $detail['cate_id2'];
+            }
+            if ($detail['cate_id3']) {
+              //$supplierModel->cate_id3 = $detail['cate_id3'];
+            }
+            if ($level_id) {
+              $supplierModel->level = $level_id;
+            }
+            $supplierModel->cooperate = 2;
+            $supplierModel->save();
+            echo "评价id{$list['id']}-成功".PHP_EOL;
+          } else {
+            echo "评价id{$list['id']}-不成功".PHP_EOL;
+          }
+        }
+      }
+    }
+    echo "总数为{$count}".PHP_EOL;
+  }
+
 }
